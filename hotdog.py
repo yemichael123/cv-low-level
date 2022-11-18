16af23e36afdcfa0daf4edc39ef7102c378c3294
yemichael123 <59985956+yemichael123@users.noreply.github.com>
2022-11-17 19:01:53 -0500

    Update hotdog.py

--- hotdog.py
+++ hotdog.py
@@ -6,19 +6,22 @@ from geometry_msgs.msg import Point, Twist
 from math import atan2
 
 movementPredThres = 50
-speedPub = rospy.Publisher("/jackal_velocity_control/cmd_vel", Twist, queue_size = 1)
+speedPub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size = 10)
 speed = Twist()
 
 roadPercentage = 0
 
 def getPredVal(msg):
     global roadPercentage
+    print(f"pred value: {msg.data}")
     roadPercentage = msg.data
 
-
-def adjust_movement(x, y) :
+def adjust_movement(x, y):
+    global speed
+    global speedPub
     speed.linear.x = x
     speed.angular.z = y
+    print(f"velocity set to: {x}, {y}")
     speedPub.publish(speed)
 
 def velocity_control(moveForward):
@@ -26,19 +29,25 @@ def velocity_control(moveForward):
     #   adjustMovement(4, 0)
     # else:
     #   adjustMovement(0, 4)
-    adjustMovement(4 if moveForward else 0, 0 if moveForward else 4)
+    #print("setting movement...")
+    adjust_movement(.2 if moveForward else 0, 0 if moveForward else .2)
 
 def main():
+    rospy.init_node("cv_low_level")
     # may have to replace while(true) with some rate limit
     global roadPercentage
     global movementPredThres
     sub = rospy.Subscriber("/cv_nav/road_percentage", Float64, getPredVal)
-    moveForwardOld = roadPercentage > movementPredThres
+    r = rospy.Rate(40)
+    #moveForwardOld = roadPercentage > movementPredThres
+    #velocity_control(True)
     while(True):
-        moveForwardNew = roadPercentage > movementPredThres
-        if (moveForwardOld != moveForwardNew):
-            velocity_control(moveForwardNew)
-        moveForwardOld = moveForwardNew
+        # print(f"in while pred value: {roadPercentage}")
+        #moveForwardNew = roadPercentage > movementPredThres
+        #if (moveForwardOld != moveForwardNew):
+        moveForward = roadPercentage > movementPredThres
+        velocity_control(moveForward)
+        #moveForwardOld = moveForwardNew
 
 if __name__ == "__main__":
     main()
